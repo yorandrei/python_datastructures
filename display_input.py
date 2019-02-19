9354e374485d9f4b1b829466ebcbcfd41b43cfd3
Andrei Kniazev <andrei.kniazev@yorlabs.com>
2019-02-19 01:18:08 +0000

    Sliders working

--- display_input.py
+++ display_input.py
@@ -4,10 +4,11 @@ import numpy as np
 from numpy import genfromtxt
 from matplotlib.widgets import Slider
 
-skiph = 200
-skipf = 200
+filename = 'data/fpga_in_43.csv'
+skiph = 1
+skipf = 0
 
-data = genfromtxt('data/fpga_in_20.csv', 
+data = genfromtxt(filename, 
                     delimiter=',', 
                     skip_header=skiph, 
                     skip_footer=skipf,
@@ -28,7 +29,29 @@ axcolor = 'lightgoldenrodyellow'
 axstart = plt.axes([0.25, 0.15, 0.65, 0.03], facecolor=axcolor)
 axstop = plt.axes([0.25, 0.1, 0.65, 0.03], facecolor=axcolor)
 
-slstart = Slider(axstart, "Skip Top", skiph, 1400, valinit=0, valstep=1)
-slstop = Slider(axstop, "Skip Bottom", skipf, 1400, valinit=0, valstep=1)
+slstart = Slider(axstart, "Skip Top", skiph, 700, valinit=0, valstep=1)
+slstop = Slider(axstop, "Skip Bottom", skipf, 700, valinit=0, valstep=1)
+
+def update(val):
+    skiph = int(slstart.val)
+    skipf = int(slstop.val)
+    data = genfromtxt(filename, 
+                        delimiter=',', 
+                        skip_header=skiph, 
+                        skip_footer=skipf,
+                        dtype=np.int16)
+    minVal = data.min()
+    maxVal = data.max()
+    byte_array = np.round(255.0 * (data - minVal) /
+                        (maxVal - minVal - 1.0)).astype(np.uint8)
+    imageio.imsave('data/echos.png', byte_array)
+    img = imageio.imread('data/echos.png')
+    im = ax.imshow(img)#, cmap='gray')
+    #fig.canvas.draw_idle()
+
+
+slstart.on_changed(update)
+slstop.on_changed(update)
+
 
 plt.show()
