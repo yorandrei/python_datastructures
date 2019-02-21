ae0a4ca92ed2695de2419c781f92fb199878ab12
Andrei Kniazev <andrei.kniazev@yorlabs.com>
2019-02-20 17:30:40 -0800

    Add buttons and text field

--- file_selector.py
+++ file_selector.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from tkinter import filedialog
+import os
 
 PROGRAM_NAME = ' File Inspector '
 
@@ -16,12 +18,62 @@ class Inspector:
 # Initially the text field will display the list of files in current directory
 # When user selects the file to read it is previewed in text field instead.
 # When user clicks "display data" the plt displays it
-    def open_file(self):
-        pass
-        #input_file_name =  
+    def open_file(self, event):
+        input_file_name = self.file_name_field.get()
+        #input_file_name = os.path.basename()
+        print(input_file_name)
+
+    def browse(self, event):
+        print("Browsing")
+        ftypes = [('CSV Files)', '*.csv'), ('Excel Fiels', '*.xlsx')]
+        dlg = filedialog.Open(self, filetypes=ftypes)
+        fl = dlg.show()
+
+    def setup_fileselect(self):
+        top_frame = Frame(root)
+        top_frame.pack(anchor='w', fill='x', padx=5, pady=2)
+        #top_frame.pack(anchor='w', fill='x', expand='yes')
+
+        Label(top_frame, text='Select Input File').pack(side=LEFT, padx=3, pady=5)
+        self.input_file_name = StringVar()
+
+        self.file_name_field = Entry(top_frame, textvariable=self.input_file_name)
+        self.file_name_field.pack(side=LEFT, fill='x', expand='yes')
+        self.file_name_field.bind("<Return>", self.open_file)
+
+        photo = PhotoImage(file="resources/arrow_icon2_20.png")
+        self.browse_button = Label(top_frame, image=photo)
+        self.browse_button.image = photo
+        self.browse_button.config(image=photo, width="20", height="20")
+        self.browse_button.bind("<Button-1>", self.browse)
+        self.browse_button.bind("<Return>", self.browse)
+        self.browse_button.pack(side=LEFT, anchor='w')
+
+        self.open_button = Button(top_frame, text='Open')
+        self.open_button.pack(side=RIGHT, padx=3, pady=5)
+        self.open_button.bind("<Button-1>", self.open_file)
+        self.open_button.bind("<Return>", self.open_file)
+
+
+
+    def setup_text(self):
+        self.content_text = Text(root, wrap='none')
+        self.content_text.pack(expand='yes', fill='both')
+
+        self.yscroll_bar = Scrollbar(self.content_text)
+        self.content_text.configure(yscrollcommand=self.yscroll_bar.set)
+        self.yscroll_bar.config(command=self.content_text.yview)
+        self.yscroll_bar.pack(side='right', fill='y')
+
+        self.xscroll_bar = Scrollbar(self.content_text, orient=HORIZONTAL)
+        self.content_text.configure(xscrollcommand=self.xscroll_bar.set)
+        self.xscroll_bar.config(command=self.content_text.xview)
+        self.xscroll_bar.pack(side='bottom', fill='x')
 
     def init_gui(self):
-        self.content_text = Text(root, wrap='none')
+        self.setup_fileselect()
+        self.setup_text()
+
 
 if __name__ == '__main__':
     root = Tk()
