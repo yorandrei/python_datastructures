4d0c547abcd6286e6b87030943ebdb2c7fe4f793
Andrei Kniazev <andrei.kniazev@yorlabs.com>
2019-02-21 17:24:30 +0000

    Fix open / display functions

--- file_selector.py
+++ file_selector.py
@@ -18,16 +18,13 @@ class Inspector:
 # Initially the text field will display the list of files in current directory
 # When user selects the file to read it is previewed in text field instead.
 # When user clicks "display data" the plt displays it
-    def open_file(self, event):
-        input_file_name = self.file_name_field.get()
-        #input_file_name = os.path.basename()
-        print(input_file_name)
-        self.content_text.delete(1.0, END)
-        with open(input_file_name) as _file:
-            self.content_text.insert(1.0, _file.read())
+# TODO: Display directory contents in textbox on boot
+# TODO: Explore if it's better to display inside gui or outside
 
-    def browse(self, event):
-        print("Browsing")
+    def display(self, event):
+        print("Calling Display")
+
+    def open(self, event):
         ftypes = [('CSV Files)', '*.csv'), 
                 ('Excel Fiels', '*.xlsx'),
                 ('All', '*')]
@@ -36,6 +33,34 @@ class Inspector:
         if not self.file_path:
             return
         self.input_file_name.set(self.file_path)
+
+        self.content_text.delete(1.0, END)
+        with open(self.file_path) as _file:
+            self.content_text.insert(1.0, _file.read())
+
+    def show_contents(self):
+        cwd = os.getcwd()
+        contents = os.listdir()
+        self.input_file_name.set(cwd)
+
+        self.content_text.delete(1.0, END)
+        #self.content_text.insert(1.0, contents)
+        for f in contents:
+            self.content_text.insert(END, f)
+            self.content_text.insert(END, '\n')
+
+    def open_file(self, event):
+        file_name = self.input_file_name.get()
+        if not file_name:
+            # TODO: Change into a pop-up window
+            print("Please specify file name")
+        # Check if file opens
+        cwd = os.getcwd()
+        infile = os.listdir()
+        print(infile)
+        #elif
+        #else:
+        #    print("File exists")
 
 
     def setup_fileselect(self):
@@ -54,14 +79,14 @@ class Inspector:
         self.browse_button = Label(top_frame, image=photo)
         self.browse_button.image = photo
         self.browse_button.config(image=photo, width="20", height="20")
-        self.browse_button.bind("<Button-1>", self.browse)
-        self.browse_button.bind("<Return>", self.browse)
+        self.browse_button.bind("<Button-1>", self.open)
+        self.browse_button.bind("<Return>", self.open)
         self.browse_button.pack(side=LEFT, anchor='w')
 
-        self.open_button = Button(top_frame, text='Open')
+        self.open_button = Button(top_frame, text='Display')
         self.open_button.pack(side=RIGHT, padx=3, pady=5)
-        self.open_button.bind("<Button-1>", self.open_file)
-        self.open_button.bind("<Return>", self.open_file)
+        self.open_button.bind("<Button-1>", self.display)
+        self.open_button.bind("<Return>", self.display)
 
 
 
@@ -78,6 +103,8 @@ class Inspector:
         self.content_text.configure(xscrollcommand=self.xscroll_bar.set)
         self.xscroll_bar.config(command=self.content_text.xview)
         self.xscroll_bar.pack(side='bottom', fill='x')
+
+        self.show_contents()
 
     def init_gui(self):
         self.setup_fileselect()
